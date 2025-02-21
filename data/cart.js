ec74e34a95aab88a7ee7ea7e6664905f58f60320
Vikas <ppsb1212@gmail.com>
2025-02-21 16:09:58 +0530

    Use modules and import cart from cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-const cart = [{
+export const cart = [{
   product: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
   quantity: 1
 },{
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import { cart } from "../data/cart.js";
+
 let renderProducts = '';
 products.forEach( product => {
   const html = `
